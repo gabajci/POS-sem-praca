4bfada0bc0f852e97cbc5811e14edb4da034588b
parziyval <el.luteci@gmail.com>
2022-01-08 16:11:42 +0100

    uprava h suborov

--- client.h
+++ client.h
@@ -2,6 +2,7 @@
 #define SEMKAPONG_CLIENT_H
 
 void displayPaddle(WINDOW * win, int y, int x);
+void displayBall(WINDOW * win, int y, int x);
 void* prenos_func (void* data);
 void* plocha_func(void* data);
 
--- server.h
+++ server.h
@@ -2,9 +2,10 @@
 #define SEMKAPONG_SERVER_H
 
 void displayPaddle(WINDOW * win, int y, int x);
+void displayBall(WINDOW * win, int y, int x);
 int getZacSmer(int sucetSkore);
 void *logika_func (void* data);
 void* prenos_func (void* data);
-void* zobraz_func(void* data);
+void* plocha_func(void* data);
 
 #endif //SEMKAPONG_SERVER_H
